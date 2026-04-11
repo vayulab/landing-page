048218fd4b672fa3971c7da63f8697d5adaacfc9
dmx <dmwx66@protonmail.com>
2026-04-11 14:31:01 -0300

    web analytics correction

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,12 @@ import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
-import AnalyticsGuide from "./pages/AnalyticsGuide";
+import { Analytics } from "@vercel/analytics/react";
 
 function Router() {
   return (
     <Switch>
       <Route path={"/"} component={Home} />
-      <Route path={"/analytics-guide"} component={AnalyticsGuide} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
@@ -34,6 +33,7 @@ function App() {
         <TooltipProvider>
           <Toaster />
           <Router />
+          <Analytics />
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
